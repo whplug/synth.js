60237a9dda15de64f82f56e779df9ed8d0f4f5cd
whplug <whplug@gmail.com>
2024-03-09 17:36:26 +0100

    Button

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<script src="script.js"></script>
+document.querySelector('.play').addEventListener('click', () => {
+    const actx = new (AudioContext || webkitAudioContext)();
+    if (!actx) throw 'Not supported :(';
+    const osc = actx.createOscillator();
+    osc.type = 'sawtooth';
+    osc.frequency.value = 440;
+    osc.connect(actx.destination);
+    osc.start();
+    osc.stop(actx.currentTime + 2);
+});
