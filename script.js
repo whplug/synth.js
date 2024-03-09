48571525884492f57b7b1fd7c2e1f118f6f772b8
whplug <whplug@gmail.com>
2024-03-09 17:44:37 +0100

    Drone

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
 document.querySelector('.play').addEventListener('click', () => {
     const actx = new (AudioContext || webkitAudioContext)();
     if (!actx) throw 'Not supported :(';
     const osc = actx.createOscillator();
     osc.type = 'sawtooth';
-    osc.frequency.value = 440;
+    osc.frequency.value = 30;
     osc.connect(actx.destination);
     osc.start();
-    osc.stop(actx.currentTime + 2);
+    osc.stop(actx.currentTime + 10);
+});
 });
